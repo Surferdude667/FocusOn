2e095fbef98f29502d456025283671cf8df88985
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2020-03-18 13:00:36 +0100

    TableView added

--- FocusOn/Controller/TodayController.swift
+++ FocusOn/Controller/TodayController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class TodayController: UIViewController {
-
+class TodayController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //  MARK:- TableView
+    
+    //  Return the number of sections in table.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    
+    //  Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    //  Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell1 = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        
+        cell1.textLabel?.text = "Cell text"
+        return cell1
+    }
+    
+    
+    
+    func configure() {
+        print("Today")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Today")
+        configure()
     }
 
 }
