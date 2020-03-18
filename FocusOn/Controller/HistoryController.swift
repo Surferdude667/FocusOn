21f1e9bbbd66c384776d28c3ef81a2103c9e9d4c
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2020-03-18 09:35:17 +0100

    TabBar Setup

---
+++ FocusOn/Controller/HistoryController.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryViewController.swift
+//  FocusOn
+//
+//  Created by Bjørn Lau Jørgensen on 18/03/2020.
+//  Copyright © 2020 Bjørn Lau Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class HistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("History")
+    }
+}
---
+++ FocusOn/Controller/ProgressController.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressController.swift
+//  FocusOn
+//
+//  Created by Bjørn Lau Jørgensen on 18/03/2020.
+//  Copyright © 2020 Bjørn Lau Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class ProgressController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Progress")
+    }
+}
