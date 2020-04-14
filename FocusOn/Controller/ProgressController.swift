80c4e2edbc7d66fe284258d13994a5aa4b28a2e3
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2020-04-14 12:38:25 +0200

    Basic piechart implemented

--- FocusOn/Controller/ProgressController.swift
+++ FocusOn/Controller/ProgressController.swift
@@ -7,11 +7,42 @@
 //
 
 import UIKit
+import Charts
 
 class ProgressController: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    var completedGoalsEntry = PieChartDataEntry(value: 10)
+    var unfinishedGoalsEntry = PieChartDataEntry(value: 5)
+    
+    var numberOfEntries = [PieChartDataEntry]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pieChart.chartDescription?.text = "Description"
+        
+        completedGoalsEntry.label = "Completed"
+        unfinishedGoalsEntry.label = "Unfinished"
+        
+        numberOfEntries = [completedGoalsEntry, unfinishedGoalsEntry]
+        
+        updateChartData()
     }
+    
+    
+    func updateChartData() {
+        let chartDataSet = PieChartDataSet(entries: numberOfEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor.green, UIColor.red]
+        chartDataSet.colors = colors
+        
+        pieChart.data = chartData
+    }
+    
 }
